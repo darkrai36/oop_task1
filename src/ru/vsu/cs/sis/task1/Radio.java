55096d916f17beb6bb6b8080d0d0612175d4ae3a
Igor S. <sismain36@gmail.com>
2024-09-27 23:33:19 +0300

    Add class Radio for my task

---
+++ src/ru/vsu/cs/sis/task1/Radio.java
@@ -0,0 +1,11 @@
+package ru.vsu.cs.sis.task1;
+
+public class Radio {
+    public static void sayHello(String message) {
+        System.out.println("Hello! " + message);
+    }
+
+    public static void main(String[] args) {
+        sayHello("all might");
+    }
+}
