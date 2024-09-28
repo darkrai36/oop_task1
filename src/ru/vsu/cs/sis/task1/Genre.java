4c94eb38880d223f667c33d6ed8d764de2b285ef
Igor S. <sismain36@gmail.com>
2024-09-28 10:34:51 +0300

    Changed class 'Video', add fields and methods for class 'Genre'

--- src/ru/vsu/cs/sis/task1/Genre.java
+++ src/ru/vsu/cs/sis/task1/Genre.java
@@ -1,4 +1,19 @@
 package ru.vsu.cs.sis.task1;
 
 public class Genre {
+    private String name; //Название жанра
+    private int rating; // Его рейтинг
+
+    public Genre(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
--- src/ru/vsu/cs/sis/task1/Video.java
+++ src/ru/vsu/cs/sis/task1/Video.java
@@ -15,25 +15,14 @@ public class Video {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getDurationInSeconds() {
         return durationInSeconds;
-    }
-
-    public void setDurationInSeconds(int durationInSeconds) {
-        this.durationInSeconds = durationInSeconds;
     }
 
     public String getLink() {
         return link;
     }
 
-    public void setLink(String link) {
-        this.link = link;
-    }
     public void play() {
         System.out.println("Now plays: " + name.toString() + ". It plays " + durationInSeconds + "seconds.");
     }
