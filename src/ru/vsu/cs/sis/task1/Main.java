af50afe0c4500e504ee06501b49a8a891f06e3a2
Igor S. <sismain36@gmail.com>
2024-09-28 10:56:13 +0300

    Add test in class 'Main'. Program works correctly

--- src/ru/vsu/cs/sis/task1/Main.java
+++ src/ru/vsu/cs/sis/task1/Main.java
@@ -2,6 +2,9 @@ package ru.vsu.cs.sis.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Video video = new Video("урок как создать папку на рабочем столе", "kriper2004",
+                16409, "https://youtu.be/5QZcOugHQ6s?si=EYR31YTes7FDr__N");
+        video.play();
+        video.pause();
     }
 }
