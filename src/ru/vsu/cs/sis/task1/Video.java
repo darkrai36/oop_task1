c30eb28fca9b0743bac9224339a86ee40672855a
Igor S. <sismain36@gmail.com>
2024-09-28 10:59:00 +0300

    Add new field 'author' in class 'Video'. Change method 'play()'for new logic.

--- src/ru/vsu/cs/sis/task1/Video.java
+++ src/ru/vsu/cs/sis/task1/Video.java
@@ -1,12 +1,14 @@
 package ru.vsu.cs.sis.task1;
 
 public class Video {
-    private String name; //Название видео
+    private String name;//Название видео
+    private String author;
     private int durationInSeconds; //Длительность в секундах
     private String link; //Ссылка на видео
 
-    public Video(String name, int durationInSeconds, String link) {
+    public Video(String name, String author, int durationInSeconds, String link) {
         this.name = name;
+        this.author = author;
         this.durationInSeconds = durationInSeconds;
         this.link = link;
     }
@@ -14,18 +16,19 @@ public class Video {
     public String getName() {
         return name;
     }
-
+    public String getAuthor() {return author;}
     public int getDurationInSeconds() {
         return durationInSeconds;
     }
-
     public String getLink() {
         return link;
     }
 
     public void play() {
-        System.out.println("Now plays: " + name.toString() + ". It plays " + durationInSeconds + "seconds.");
+        System.out.println("Now plays: '" + name + "' from " + author + ". It plays " + durationInSeconds + " seconds.");
+        System.out.println("Link on this video: " + link);
     }
+
     public void pause() {
         System.out.println("You get a pause. Please, tap on 'Play' again to continue watch video!");
     }
