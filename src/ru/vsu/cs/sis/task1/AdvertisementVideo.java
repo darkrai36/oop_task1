021aef53db20c60154e7798e3da8d93a9ea0719a
Igor S. <sismain36@gmail.com>
2024-09-28 13:02:06 +0300

    Add class 'AdvertisementVideo' and logic for it.

---
+++ src/ru/vsu/cs/sis/task1/AdvertisementVideo.java
@@ -0,0 +1,15 @@
+package ru.vsu.cs.sis.task1;
+
+public class AdvertisementVideo extends Video {
+    private String company;
+
+    public AdvertisementVideo(String name, String channel, int durationInSeconds, String link, Genre genre, String company) {
+        super(name, channel, durationInSeconds, link, genre);
+        this.company = company;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing advertisement for " + company + ": " + super.getName() + " (" + super.getGenre().getName() + ") for " + super.getDurationInSeconds() + " seconds.");
+    }
+}
