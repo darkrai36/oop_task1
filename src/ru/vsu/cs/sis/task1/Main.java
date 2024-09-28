75e000c4af7f7ccf14fad0f732313e037001887c
Igor S. <sismain36@gmail.com>
2024-09-28 11:12:22 +0300

    Write method for splitting my output text. Also test class 'Bid'.

--- src/ru/vsu/cs/sis/task1/Main.java
+++ src/ru/vsu/cs/sis/task1/Main.java
@@ -1,10 +1,24 @@
 package ru.vsu.cs.sis.task1;
 
 public class Main {
+    public static void makeSplitter() {
+        System.out.println("===============================================");
+    }
     public static void main(String[] args) {
         Video video = new Video("урок как создать папку на рабочем столе", "kriper2004",
                 16409, "https://youtu.be/5QZcOugHQ6s?si=EYR31YTes7FDr__N");
         video.play();
+        makeSplitter();
+
         video.pause();
+        makeSplitter();
+
+        Bid bid = new Bid("x.com", 67, "кто прочитал тот хороший человек");
+        bid.printMessage();
+        makeSplitter();
+
+        Bid importantBid = new Bid("vk.com", 658938, "заходит улитка в бар...");
+        importantBid.printMessage();
+        makeSplitter();
     }
 }
