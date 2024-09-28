29859a63a77634dda59813f972b3d8f20256ca74
Igor S. <sismain36@gmail.com>
2024-09-28 10:45:04 +0300

    Add logic for class 'Bid'

--- src/ru/vsu/cs/sis/task1/Bid.java
+++ src/ru/vsu/cs/sis/task1/Bid.java
@@ -1,4 +1,41 @@
 package ru.vsu.cs.sis.task1;
 
 public class Bid {
+    private String customerName;
+    private static final int minPriceForMessage = 5000;
+    private int priceFromCustomer;
+    private String message;
+    private boolean isImportant;
+
+    public Bid(String customerName, int priceFromCustomer, String message) {
+        this.customerName = customerName;
+        this.priceFromCustomer = priceFromCustomer;
+        this.message = message;
+        if (priceFromCustomer > minPriceForMessage) {
+            this.isImportant = true;
+        }
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public int getPriceFromCustomer() {
+        return priceFromCustomer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+    public void printMessage() {
+        if (isImportant) {
+            System.out.println("Срочное объявление от " + customerName + "!" + message);
+        } else {
+            System.out.println("Hello, dear user! We get a new bid from " + customerName + ". The message: '" + message + "'.");
+        }
+    }
 }
